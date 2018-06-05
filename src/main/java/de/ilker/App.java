bb5064ae3552fc065aee97dbd69174d0301d537e
Ilker Coban <ilkercoban2@hotmail.de>
2018-06-05 14:27:13 +0200

    erster commit

---
+++ src/main/java/de/ilker/App.java
@@ -0,0 +1,13 @@
+package de.ilker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
